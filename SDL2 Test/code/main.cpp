942864ddb4f40b6c594686d658b869df7b2ee5fb
potrox <gzempf@mail.ru>
2023-04-17 02:52:23 +0700

    Revert "merge file"
    This reverts commit 40283f5242e8d71e304794e26b6faa2a60ec10d6.

--- SDL2 Test/code/main.cpp
+++ SDL2 Test/code/main.cpp
@@ -6,9 +6,7 @@
 
 const int WIDTH = 800, HEIGHT = 600;
 SDL_Window* window = NULL;
-SDL_Surface* surface = NULL;
-SDL_Renderer* ren = NULL;
-
+SDL_Surface* screen_surface = NULL;
 
 int init()
 {
@@ -17,7 +15,7 @@ int init()
         return 1;
     }
 
-    window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window* window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 
     // Check that the window was successfully created
     if (NULL == window)
@@ -27,30 +25,14 @@ int init()
         return 1;
     }
 
-    ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Surface* screen_surface = SDL_GetWindowSurface(window);
 
-    if (ren == NULL) {
-        cout << "Can't create renderer: " << SDL_GetError() << endl;
-    }
+    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 51, 51, 51));
+
+    SDL_Event windowEvent;
 
     SDL_UpdateWindowSurface(window);
 
-    return 0;
-}
-
-int draw()
-{
-    SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
-
-    SDL_Rect rect1 = { 10, 10, 50, 50 };
-
-    for (int i = 10; i <= 800 - 10; i += 4)
-    {
-        for (int j = 10; j <= 600 - 10; j += 4)
-        {
-            SDL_RenderDrawPoint(ren, i, j);
-        }
-    }
     return 0;
 }
 
@@ -66,23 +48,24 @@ void check_for_close()
             {
                 break;
             }
+
+
+
         }
+
     }
 }
 
 int exit()
 {
     SDL_DestroyWindow(window);
-    window = NULL;
-    SDL_DestroyRenderer(ren);
-    ren = NULL;
     SDL_Quit();
     return 0;
 }
 
 int main(int argc, char* argv[])
 {
-   /* int size = 10;
+    int size = 10;
 
     AreaSize(size);
 
@@ -99,13 +82,9 @@ int main(int argc, char* argv[])
             cout << world[i][j].tileStatus << " ";
         }
         cout << endl;
-    }*/
+    }
 
     init();
-
-    draw();
-
-    SDL_RenderPresent(ren);
 
     check_for_close();
     
