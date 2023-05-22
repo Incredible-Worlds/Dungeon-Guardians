194e5be02b6201e3dfcf14067eef6c80577bc86d
KamysDev <sereginka070504@gmail.com>
2023-05-23 01:21:18 +0700

    Fixed inc in mainmenu

--- Main Game/Headers/MainMenu.h
+++ Main Game/Headers/MainMenu.h
@@ -2,6 +2,6 @@
 
 #include "GameModule.h"
 #include "../../SDL2-2.0.9/include/SDL.h"
-#include <SDL_mixer.h>
+#include "../../SDL2-2.0.9/include/SDL_mixer.h"
 
 int menu_main(SDL_Window* window, SDL_Surface* surface, SDL_Renderer* ren, Mix_Music* mainmusic);
